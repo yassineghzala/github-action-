d16a893a0b578c5df20a6bc29be09dc282271810
Yassine Ghzala <114862724+yassineghzala@users.noreply.github.com>
2026-03-06 03:23:16 +0100

    test

--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { User } from '../models/User';
 })
 export class UserService {
 
-  baseURL:string="http://localhost:8081/user"
+  baseURL:string="http://spring-boot-app:8081/user"
   constructor(private  httpClient:HttpClient) { }
 
   getAllUsers():Observable<any>{
