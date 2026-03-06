a2884f634fa92dedb6d8d1426dd39ca427fb8352
Yassine Ghzala <114862724+yassineghzala@users.noreply.github.com>
2026-03-06 03:21:36 +0100

    test

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Add your allowed origins here
+                .allowedOrigins("http://angular-app:4200") // Add your allowed origins here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
